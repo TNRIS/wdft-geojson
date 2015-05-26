ddaa63efbd4c25033694e55497633cb6f37807c6
wilsaj <wilson.andrew.j+github@gmail.com>
2015-05-26 12:34:54 -0500

    package conversion stream up as part of this module

--- examples/generate-current.js
+++ examples/generate-current.js
@@ -2,48 +2,12 @@
 var fs = require('fs');
 
 var JSONStream = require('JSONStream');
-var geojson = require('geojson');
-var R = require('ramda');
 var request = require('request');
-var through = require('through');
-
+var WDFTGeojson = require('wdft-geojson');
 
 
 request.get('http://waterdatafortexas.org/reservoirs/recent-conditions.json')
   .pipe(JSONStream.parse())
-  .pipe(through(function write(conditions) {
-    var stream = this;
-
-    var flat = R.toPairs(conditions)
-      .map(function (pair) {
-        var obj = pair[1];
-        var location = obj.gauge_location;
-        obj.lng = location.coordinates[0];
-        obj.lat = location.coordinates[1];
-        delete obj.gauge_location;
-        return R.merge({name: pair[0]}, obj);
-      })
-
-    var include = [
-      "name",
-      "area",
-      "condensed_name",
-      "conservation_capacity",
-      "conservation_pool_elevation",
-      "conservation_storage",
-      "dead_pool_elevation",
-      "elevation",
-      "full_name",
-      "percent_full",
-      "short_name",
-      "timestamp",
-      "volume",
-      "volume_under_conservation_pool_elevation",
-    ];
-
-    var geojsonConditions = geojson.parse(flat, {Point: ['lat', 'lng']}, include);
-    stream.emit('data', geojsonConditions);
-  }))
+  .pipe(WDFTGeojson.stream())
   .pipe(JSONStream.stringify())
   .pipe(process.stdout);
-
