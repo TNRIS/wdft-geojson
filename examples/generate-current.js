f86060b10efb2ffe5c67e89051e17ce8bbe1742a
wilsaj <wilson.andrew.j+github@gmail.com>
2015-05-26 13:13:44 -0500

    slight stylistic change to example

--- examples/generate-current.js
+++ examples/generate-current.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 
 var JSONStream = require('JSONStream');
 var request = require('request');
-var WDFTGeojson = require('wdft-geojson');
+var WDFTGeoJSON = require('wdft-geojson');
 
 
 request.get('http://waterdatafortexas.org/reservoirs/recent-conditions.json')
   .pipe(JSONStream.parse())
-  .pipe(WDFTGeojson.stream())
+  .pipe(WDFTGeoJSON.stream())
   .pipe(JSONStream.stringify())
   .pipe(process.stdout);
