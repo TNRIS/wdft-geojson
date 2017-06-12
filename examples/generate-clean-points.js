bdbda7119c9dbf235140599ca5c25a5ff9b9011b
adambreznicky <smudgeservices@gmail.com>
2017-06-12 14:17:52 -0500

    updated for lambda with export to s3

--- examples/generate-clean-points.js
+++ examples/generate-clean-points.js
@@ -7,58 +7,77 @@ var R = require('ramda');
 var through = require('through');
 var WDFTGeoJSON = require('../lib/index.js');
 
+var AWS = require('aws-sdk');
+var s3UploadStream = require('s3-upload-stream');
 
-request.get('http://waterdatafortexas.org/reservoirs/recent-conditions.json')
-  .pipe(JSONStream.parse())
-  .pipe(WDFTGeoJSON.stream())
-  .pipe(WDFTGeoJSON.style())
-  .pipe(through(function write(geojson) {
-    var renameProperties = {
-      full_name: 'Name',
-      timestamp: 'Date',
-      percent_full: 'Percent Full',
-      elevation: 'Water Surface Elevation',
-      volume: 'Reservoir Storage',
-      conservation_pool_elevation: 'Conservation Pool Elevation',
-      conservation_storage: 'Conservation Storage',
-      conservation_capacity: 'Conservation Capacity',
-      area: 'Surface Area',
-      'marker-color': 'marker-color'
-    };
+exports.handler = function(event, context) {
+  var s3Stream = s3UploadStream(new AWS.S3());
+  var uploader = s3Stream.upload({
+    ACL: 'public-read',
+    Bucket: 'tnris-public-data',
+    Key: 'wdft/reservoir-recent-conditions-points.geojson'
+  });
 
-    geojson.features.forEach(function rename(feature) {
-      var urlName = feature.properties['short_name'].toLowerCase().replace(/ /g, '-');
+  uploader.on('uploaded', function (stats) {
+    console.log('Upload stats: ', stats);
+  });
 
-      R.toPairs(renameProperties)
-        .forEach(function (pair) {
-          var from = pair[0];
-          var to = pair[1];
-          feature.properties[to] = feature.properties[from];
-          if (from !== to) {
-            delete feature.properties[from];
-          }
-        });
+  uploader.on('error', function (e) {
+    console.log('Upload error: ', e);
+  });
 
-      feature.properties = R.pick(R.values(renameProperties), feature.properties);
-
-      var urlBase = 'http://waterdatafortexas.org/reservoirs/individual/' + urlName;
-
-      var reservoirUrls = {
-        'Reservoir Page': urlBase,
-        'Recent Graph': urlBase + '/recent-volume@2x.png',
-        'Historical Graph': urlBase + '/historical-volume@2x.png',
-        'Statistics Graph': urlBase + '/recent-storage-statistics@2x.png'
+  request.get('http://waterdatafortexas.org/reservoirs/recent-conditions.json')
+    .pipe(JSONStream.parse())
+    .pipe(WDFTGeoJSON.stream())
+    .pipe(WDFTGeoJSON.style())
+    .pipe(through(function write(geojson) {
+      var renameProperties = {
+        full_name: 'Name',
+        timestamp: 'Date',
+        percent_full: 'Percent Full',
+        elevation: 'Water Surface Elevation',
+        volume: 'Reservoir Storage',
+        conservation_pool_elevation: 'Conservation Pool Elevation',
+        conservation_storage: 'Conservation Storage',
+        conservation_capacity: 'Conservation Capacity',
+        area: 'Surface Area',
+        'marker-color': 'marker-color'
       };
 
-      R.toPairs(reservoirUrls)
-        .forEach(function (pair) {
-          var name = pair[0];
-          var url = pair[1];
-          feature.properties[name] = url;
-        });
-    });
+      geojson.features.forEach(function rename(feature) {
+        var urlName = feature.properties['short_name'].toLowerCase().replace(/ /g, '-');
 
-    this.emit('data', geojson);
-  }))
-  .pipe(JSONStream.stringify(false))
-  .pipe(process.stdout);
+        R.toPairs(renameProperties)
+          .forEach(function (pair) {
+            var from = pair[0];
+            var to = pair[1];
+            feature.properties[to] = feature.properties[from];
+            if (from !== to) {
+              delete feature.properties[from];
+            }
+          });
+
+        feature.properties = R.pick(R.values(renameProperties), feature.properties);
+
+        var urlBase = 'http://waterdatafortexas.org/reservoirs/individual/' + urlName;
+
+        var reservoirUrls = {
+          'Reservoir Page': urlBase,
+          'Recent Graph': urlBase + '/recent-volume@2x.png',
+          'Historical Graph': urlBase + '/historical-volume@2x.png',
+          'Statistics Graph': urlBase + '/recent-storage-statistics@2x.png'
+        };
+
+        R.toPairs(reservoirUrls)
+          .forEach(function (pair) {
+            var name = pair[0];
+            var url = pair[1];
+            feature.properties[name] = url;
+          });
+      });
+
+      this.emit('data', geojson);
+    }))
+    .pipe(JSONStream.stringify(false))
+    .pipe(uploader);
+};
--- examples/generate-clean-polygons.js
+++ examples/generate-clean-polygons.js
@@ -11,79 +11,97 @@ var minimist = require('minimist');
 
 var WDFTGeoJSON = require('../lib/index.js');
 
+var AWS = require('aws-sdk');
+var s3UploadStream = require('s3-upload-stream');
+
 
 var DEFAULT_RESERVOIRS_PATH = path.resolve(__dirname, '../data/reservoirs-simplified.geojson');
 var argv = minimist(process.argv.slice(2));
 var reservoirs_geojson_file = argv._.length ? argv._[0] : DEFAULT_RESERVOIRS_PATH;
 
-fs.createReadStream(reservoirs_geojson_file)
-  .pipe(JSONStream.parse())
-  .pipe(es.writeArray(function(err, array) {
-    var reservoirs = {};
+exports.handler = function(event, context) {
+  fs.createReadStream(reservoirs_geojson_file)
+    .pipe(JSONStream.parse())
+    .pipe(es.writeArray(function(err, array) {
+      var reservoirs = {};
 
-    array[0].features.forEach(function(reservoir) {
-      reservoirs[reservoir.properties.Name] = reservoir.geometry;
-    });
+      array[0].features.forEach(function(reservoir) {
+        reservoirs[reservoir.properties.Name] = reservoir.geometry;
+      });
 
-    request.get('http://waterdatafortexas.org/reservoirs/recent-conditions.json')
-      .pipe(JSONStream.parse())
-      .pipe(WDFTGeoJSON.stream({geometries: reservoirs}))
-      .pipe(WDFTGeoJSON.style())
-      .pipe(through(function write(geojson) {
-        var renameProperties = {
-          full_name: 'Name',
-          timestamp: 'Date',
-          percent_full: 'Percent Full',
-          elevation: 'Water Surface Elevation',
-          volume: 'Reservoir Storage',
-          conservation_pool_elevation: 'Conservation Pool Elevation',
-          conservation_storage: 'Conservation Storage',
-          conservation_capacity: 'Conservation Capacity',
-          area: 'Surface Area',
-          'marker-color': 'marker-color',
-          'fill': 'fill',
-          'stroke': 'stroke'
-        };
+      var s3Stream = s3UploadStream(new AWS.S3());
+      var uploader = s3Stream.upload({
+        ACL: 'public-read',
+        Bucket: 'tnris-public-data',
+        Key: 'wdft/reservoir-recent-conditions.geojson'
+      });
 
-        geojson.features.forEach(function rename(feature) {
-          var urlName = feature.properties['short_name'].toLowerCase().replace(/ /g, '-');
+      uploader.on('uploaded', function (stats) {
+        console.log('Upload stats: ', stats);
+      });
 
-          R.toPairs(renameProperties)
-            .forEach(function (pair) {
-              var from = pair[0];
-              var to = pair[1];
-              feature.properties[to] = feature.properties[from];
-              if (from !== to) {
-                delete feature.properties[from];
-              }
-            });
+      uploader.on('error', function (e) {
+        console.log('Upload error: ', e);
+      });
 
-          feature.properties = R.pick(R.values(renameProperties), feature.properties);
-
-          var urlBase = 'http://waterdatafortexas.org/reservoirs/individual/' + urlName;
-
-          var reservoirUrls = {
-            'Reservoir Page': urlBase,
-            'Recent Graph': urlBase + '/recent-volume@2x.png',
-            'Historical Graph': urlBase + '/historical-volume@2x.png',
-            'Statistics Graph': urlBase + '/recent-storage-statistics@2x.png'
+      request.get('http://waterdatafortexas.org/reservoirs/recent-conditions.json')
+        .pipe(JSONStream.parse())
+        .pipe(WDFTGeoJSON.stream({geometries: reservoirs}))
+        .pipe(WDFTGeoJSON.style())
+        .pipe(through(function write(geojson) {
+          var renameProperties = {
+            full_name: 'Name',
+            timestamp: 'Date',
+            percent_full: 'Percent Full',
+            elevation: 'Water Surface Elevation',
+            volume: 'Reservoir Storage',
+            conservation_pool_elevation: 'Conservation Pool Elevation',
+            conservation_storage: 'Conservation Storage',
+            conservation_capacity: 'Conservation Capacity',
+            area: 'Surface Area',
+            'marker-color': 'marker-color',
+            'fill': 'fill',
+            'stroke': 'stroke'
           };
 
-          R.toPairs(reservoirUrls)
-            .forEach(function (pair) {
-              var name = pair[0];
-              var url = pair[1];
-              feature.properties[name] = url;
-            });
-        });
+          geojson.features.forEach(function rename(feature) {
+            var urlName = feature.properties['short_name'].toLowerCase().replace(/ /g, '-');
 
-        //geojson.features = geojson.features.filter(function(f) {return f.geometry.type != 'Point'});
+            R.toPairs(renameProperties)
+              .forEach(function (pair) {
+                var from = pair[0];
+                var to = pair[1];
+                feature.properties[to] = feature.properties[from];
+                if (from !== to) {
+                  delete feature.properties[from];
+                }
+              });
 
-        this.emit('data', geojson);
-      }))
-      .pipe(JSONStream.stringify(false))
-      .pipe(process.stdout);
+            feature.properties = R.pick(R.values(renameProperties), feature.properties);
 
+            var urlBase = 'http://waterdatafortexas.org/reservoirs/individual/' + urlName;
+
+            var reservoirUrls = {
+              'Reservoir Page': urlBase,
+              'Recent Graph': urlBase + '/recent-volume@2x.png',
+              'Historical Graph': urlBase + '/historical-volume@2x.png',
+              'Statistics Graph': urlBase + '/recent-storage-statistics@2x.png'
+            };
+
+            R.toPairs(reservoirUrls)
+              .forEach(function (pair) {
+                var name = pair[0];
+                var url = pair[1];
+                feature.properties[name] = url;
+              });
+          });
+
+          //geojson.features = geojson.features.filter(function(f) {return f.geometry.type != 'Point'});
+          this.emit('data', geojson);
+        }))
+        .pipe(JSONStream.stringify(false))
+        .pipe(uploader);
 
   }));
+};
 
